9897c79746a9fe4f10c7c36b664daa642bcbcc59
skydr3amer <38057579+skydr3amer@users.noreply.github.com>
2022-02-25 23:27:27 -0500

    first session

---
+++ minesweeper/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minesweeper
+{
+    internal class Game
+    {
+        public Grid[][] Create(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                for (int j = 0; j < level; j++)
+            }
+        }
+    }
+}
